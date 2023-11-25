c61226fe5c447c4b2be05140298568f4f049066d
Iago C. Campos <iagoccampos@hotmail.com>
2023-11-25 20:25:50 -0300

    Resolvendo o problema de datepicker

--- src/app/shared/directives/mask.directive.ts
+++ src/app/shared/directives/mask.directive.ts
@@ -1,28 +1,65 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { formatDate } from '@angular/common'
 import { Directive, ElementRef, Inject, Input, NgZone, Optional, PLATFORM_ID, Renderer2, Self } from '@angular/core';
 import { NgControl } from '@angular/forms'
+import { MatDatepickerInput } from '@angular/material/datepicker'
+import { MatInput } from '@angular/material/input'
 import { InputMaskDirective, InputmaskOptions, createMask } from '@ngneat/input-mask'
 
 const basicMaskConfig: InputmaskOptions<any> = { autoUnmask: true }
 
-const masks = {
-	rg: createMask({ ...basicMaskConfig, mask: '9.999.999' }),
-	cpf: createMask({ ...basicMaskConfig, mask: '999.999.999-99' }),
-	phone: createMask({ ...basicMaskConfig, mask: '(99) 99999-9999' }),
-} as const
-
-export type MaskNames = keyof(typeof masks)
+type MaskNames = 'rg' | 'cpf' | 'phone' | 'date'
 
 @Directive({
 	selector: 'input[appMask]',
 })
 export class MaskDirective extends InputMaskDirective {
+	readonly matDatePickerInput: MatDatepickerInput<Date> | null = null
+
+	readonly masks: {[key in MaskNames]: InputmaskOptions<any>} = {
+		date: createMask<Date>({
+			alias: 'datetime',
+			inputFormat: 'dd/mm/yyyy',
+			parser: (value: string) => {
+				const values = value.split('/');
+				const year = +values[2];
+				const month = +values[1] - 1;
+				const date = +values[0];
+
+				return new Date(year, month, date);
+			},
+			formatter: (value: string) => {
+				const date = new Date(value)
+
+				if(this.matDatePickerInput) {
+					this.matDatePickerInput.writeValue(date)
+				}
+
+				return formatDate(date, 'dd/MM/yyyy', 'pt-BR');
+			},
+		}),
+		rg: createMask({ ...basicMaskConfig, mask: '9.999.999' }),
+		cpf: createMask({ ...basicMaskConfig, mask: '999.999.999-99' }),
+		phone: createMask({ ...basicMaskConfig, mask: '(99) 99999-9999' }),
+	} as const
+
 	@Input() set appMask(appMask: MaskNames) {
-		this.inputMask = masks[appMask]
+		this.inputMask = this.masks[appMask]
 	}
 
-	constructor(@Inject(PLATFORM_ID) platformId: string, elementRef: ElementRef<any>, renderer: Renderer2, @Optional() @Self()ngControl: NgControl | null, ngZone: NgZone) {
+	constructor(@Inject(PLATFORM_ID) platformId: string, elementRef: ElementRef<any>, renderer: Renderer2, @Optional() @Self()ngControl: NgControl | null, ngZone: NgZone, matInput: MatInput) {
 		super(platformId, elementRef, renderer, ngControl, { inputSelector: 'input', isAsync: false }, ngZone)
+
+		const inputValueAccessor = matInput['_inputValueAccessor']
+
+		if(inputValueAccessor instanceof MatDatepickerInput) {
+			this.matDatePickerInput = inputValueAccessor
+			this.matDatePickerInput.dateChange.subscribe((val) => {
+				if(val.value) {
+					ngControl?.control?.setValue(val.value, { emitEvent: false })
+				}
+			})
+		}
 	}
 }
