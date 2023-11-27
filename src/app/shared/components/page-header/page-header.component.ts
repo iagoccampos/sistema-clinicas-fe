3a41e4b588ea7af4898062d5dee3e2e3c0b473b0
Iago C. Campos <iagoccampos@hotmail.com>
2023-11-27 20:54:06 -0300

    Alteração de header

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 	selector: 'app-page-header',
 	template: `
 		<hr *ngIf="!hideHr" class="my-4">
-		<h1 class="my-4">{{ title }}</h1>
+		<h2 class="my-4">{{ title }}</h2>
 	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
