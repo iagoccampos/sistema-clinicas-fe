2fb828a56c47782b44acc14d9cf0a70b3946bb03
Iago C. Campos <iagoccampos@hotmail.com>
2023-12-25 14:24:21 -0300

    Correção de chamada dupla em clinicas

--- src/app/modules/admin/components/clinics/clinics.component.ts
+++ src/app/modules/admin/components/clinics/clinics.component.ts
@@ -1,6 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { of } from 'rxjs'
-import { catchError, ignoreElements } from 'rxjs/operators'
 import { ClinicService } from 'src/app/services/clinic.service'
 
 @Component({
@@ -11,7 +9,6 @@ import { ClinicService } from 'src/app/services/clinic.service'
 })
 export class ClinicsComponent {
 	readonly getClinics$ = this.clinicService.getClinics()
-	readonly getClinicsError$ = this.getClinics$.pipe(ignoreElements(), catchError((err) => of(err)))
 
 	constructor(private clinicService: ClinicService) { }
 }
